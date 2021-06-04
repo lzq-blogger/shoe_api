5abcad798968cab7f0b9eaf0159025dc42f131ff
lzq <20000209@qq.com>
2021-06-04 22:23:37 +0800

    6.4

---
+++ shoe_api/Controllers/productController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace shoe_api.Controllers
+{
+    public class productController : ApiController
+    {
+    }
+}
