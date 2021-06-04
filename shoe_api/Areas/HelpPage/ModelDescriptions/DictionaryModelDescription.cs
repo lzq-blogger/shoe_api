7e7a6b7ae4fe65b53493fe1c20b1a0f0a171bd89
lzq <20000209@qq.com>
2021-06-04 22:05:48 +0800

    添加项目文件。

---
+++ shoe_api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace shoe_api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ shoe_api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="shoe_api.WebApiApplication" Language="C#" %>
