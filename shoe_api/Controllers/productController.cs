ca7c0fe95b54a82356289d9d76ded0d35df4ec70
lzq <20000209@qq.com>
2021-06-05 17:20:04 +0800

    6.5

--- shoe_api/Controllers/productController.cs
+++ shoe_api/Controllers/productController.cs
@@ -29,5 +29,41 @@ namespace shoe_api.Controllers
             var json = Newtonsoft.Json.JsonConvert.SerializeObject(info);
             return json;
         }
+        //查询订单编号，用来显示在新增计划的那个下拉框里
+        //生产计划查询
+        [HttpGet]
+        public string pro_plan_order()
+        {
+            var info = from pp in db.order
+                       select new
+                       {
+                           order_id = pp.orderr_id
+                       };
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(info);
+            return json;
+        }
+        //新增生产计划
+        [HttpPost]
+        public int add_plan([FromBody] product_plan pp)
+        {
+            //返回0,1,2，用来前端调用接口的时候判断应该给用户数目提示。
+            //判断非空
+            if (pp.order_details_id.ToString() == null)
+            {
+                return 0;
+            }
+            if (pp.product_plan_num.ToString() == null ||
+              pp.operator_per == null ||
+              pp.product_time == null ||
+              pp.product_end_time == null)
+            {
+                return 1;
+            }
+            //新增数据
+            db.product_plan.Add(pp);
+            //保存数据
+            db.SaveChanges();
+            return 2;
+        }
     }
 }
